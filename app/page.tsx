96e8abf216316e71363ef4f1016b5c04f56db4d6
I3LOODZ <wachira.man@gmail.com>
2025-04-18 21:59:21 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white text-2xl">
+      BACCARAT PREDICTOR
+    </main>
+  );
+}
